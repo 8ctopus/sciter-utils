0f785792042982a6c22e96348b3b7a989ae20de3
8ctopus <hello@octopuslabs.io>
2021-08-20 11:51:32 +0500

    updated: sciter sdk to 4.4.8.7

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
-REM sciter 4.4.8.6
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/9e6f1944c27af8ac4fe41e5ce34922019fa8209c/bin/windows/x32/scapp.exe
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/9e6f1944c27af8ac4fe41e5ce34922019fa8209c/bin/windows/x32/inspector.exe
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/9e6f1944c27af8ac4fe41e5ce34922019fa8209c/bin/windows/x32/sciter.dll
+REM sciter 4.4.8.7
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/44c64a795c60147fb832cceda2a939ac8c0d7e24/bin/windows/x32/scapp.exe
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/44c64a795c60147fb832cceda2a939ac8c0d7e24/bin/windows/x32/inspector.exe
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/44c64a795c60147fb832cceda2a939ac8c0d7e24/bin/windows/x32/sciter.dll
 
 REM sciter package manager 0.1.5
 curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.1.5/spm.phar
